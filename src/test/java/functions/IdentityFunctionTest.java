94fd5181418b8dd5b3a9d67f87cf6959f93cc9a0
Vasilieva <masha.maria.vas@bk.ru>
2019-09-14 17:01:47 +0400

    Создание класса IdentityFunction и тест к нему=)

---
+++ src/test/java/functions/IdentityFunctionTest.java
@@ -0,0 +1,14 @@
+package functions;
+import org.testng.annotations.Test;
+import functions.IdentityFunction;
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+    @Test
+    public void test(){
+        IdentityFunction firstFunction = new IdentityFunction();
+        assertEquals(firstFunction.apply(5), 5, 0.00001);
+    }
+
+
+}
