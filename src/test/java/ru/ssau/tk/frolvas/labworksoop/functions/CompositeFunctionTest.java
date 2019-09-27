d8e88c0e077c2fdebd67981dc53529f45100b703
Vasilieva <masha.maria.vas@bk.ru>
2019-09-27 22:43:26 +0400

    Дополнительные тесты для сложных функций с помощью списков

--- src/test/java/ru/ssau/tk/frolvas/labworksoop/functions/CompositeFunctionTest.java
+++ src/test/java/ru/ssau/tk/frolvas/labworksoop/functions/CompositeFunctionTest.java
@@ -5,13 +5,22 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 public class CompositeFunctionTest {
+    final private double ACCURACY = 0.1;
+
     @Test
     public void test() {
-        MathFunction functionH = new IdentityFunction();
-        MathFunction functionG = new Cube();
-        MathFunction functionF = new CompositeFunction(functionH, functionG);
-        assertEquals(functionF.apply(2), 8, 0.0001);
-
+        final double ACCURACY = 0.1;
+        MathFunction cube = new Cube();
+        MathFunction sqrt = new SqrtFunction();
+        MathFunction sqr = new SqrFunction();
+        MathFunction zero = new ZeroFunction();
+        MathFunction funcF = new CompositeFunction(cube, sqrt);
+        final double[] xValues = new double[]{1, 2, 3, 4, 5, 6, 7};
+        final double[] yValues = new double[]{8, 9, 10, 11, 12, 13, 14};
+        MathFunction list = new LinkedListTabulatedFunction(xValues, yValues);
+        assertEquals(funcF.apply(5), Math.sqrt(125), ACCURACY);
+        assertEquals(list.andThen(sqrt).andThen(cube).apply(3), 5.196, ACCURACY);
+        assertEquals(list.andThen(cube).apply(20), 19683, ACCURACY);
     }
 
 
