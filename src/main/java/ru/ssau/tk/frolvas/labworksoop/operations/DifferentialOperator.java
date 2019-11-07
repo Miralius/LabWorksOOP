53670418720770ea218bcae2f4200ef21a1a00c2
Frolov <f-miralius@yandex.ru>
2019-11-08 02:18:07 +0400

    Revert "ОСТОРОЖНО!ВЫНУЖДЕНА ЗАКОММИТИТЬ НЕРАБОЧИЙ КОД((( Добавление пакета operations классы TabulatedDifferentialOperation,TabFunOperSer интерфейс DiffOperator не совпадение типов и пока не догоняю, почему..."
    This reverts commit 87d69af4

--- src/main/java/ru/ssau/tk/frolvas/labworksoop/operations/DifferentialOperator.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.frolvas.labworksoop.operations;
-
-import ru.ssau.tk.frolvas.labworksoop.functions.MathFunction;
-
-public interface DifferentialOperator<T> extends MathFunction {
-    T derive(T function);
-}
