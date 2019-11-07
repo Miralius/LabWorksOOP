38b186bf3b7034feedd500a35e69daac25abe781
Frolov <f-miralius@yandex.ru>
2019-11-08 02:18:50 +0400

    Revert "Исправление импорта в TabulatedFunction. Переписанный иттератор в LinkedListTF + тест 1-ый пункт из второй части лабы+тест"
    This reverts commit 9bac4c70

--- src/main/java/ru/ssau/tk/frolvas/labworksoop/functions/factory/TabulatedFunctionFactory.java
+++
@@ -1,7 +0,0 @@
-package ru.ssau.tk.frolvas.labworksoop.functions.factory;
-
-import ru.ssau.tk.frolvas.labworksoop.functions.TabulatedFunction;
-
-public interface TabulatedFunctionFactory {
-    TabulatedFunction create(double[] xValues, double[] yValues);
-}
