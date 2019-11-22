2856cf09a34ff699d02a2d89228a1b30316de7a6
Frolov <f-miralius@yandex.ru>
2019-11-23 01:40:44 +0400

    LW#3.3 → X task: Created main-class TabulatedFunctionFileInputStream

--- src/main/java/ru/ssau/tk/frolvas/labworksoop/io/TabulatedFunctionFileInputStream.java
+++ src/main/java/ru/ssau/tk/frolvas/labworksoop/io/TabulatedFunctionFileInputStream.java
@@ -1,18 +1,22 @@
 package ru.ssau.tk.frolvas.labworksoop.io;
 
-import ru.ssau.tk.frolvas.labworksoop.functions.TabulatedFunction;
-import ru.ssau.tk.frolvas.labworksoop.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.frolvas.labworksoop.functions.*;
+import ru.ssau.tk.frolvas.labworksoop.functions.factory.*;
+import ru.ssau.tk.frolvas.labworksoop.operations.*;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class TabulatedFunctionFileInputStream {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream("input/binary function.bin"))) {
             TabulatedFunction arrayFunc = FunctionsIO.readTabulatedFunction(inputStream, new ArrayTabulatedFunctionFactory());
             System.out.println(arrayFunc.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println("Введите размер и значения функции:");
+            System.out.println(new TabulatedDifferentialOperator().derive(FunctionsIO.readTabulatedFunction(new BufferedReader(new InputStreamReader(System.in)), new LinkedListTabulatedFunctionFactory())));
         } catch (IOException e) {
             e.printStackTrace();
         }
