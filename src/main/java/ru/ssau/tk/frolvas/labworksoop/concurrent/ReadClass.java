8ff4dfc1fb66dc9dc5f8cbb8858d4ddd808af6de
Vasilieva <masha.maria.vas@bk.ru>
2019-12-06 09:19:00 +0400

    class ReadClass

---
+++ src/main/java/ru/ssau/tk/frolvas/labworksoop/concurrent/ReadClass.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.frolvas.labworksoop.concurrent;
+
+import ru.ssau.tk.frolvas.labworksoop.functions.TabulatedFunction;
+
+public class ReadClass implements Runnable {
+    private TabulatedFunction tabulatedFunction;
+
+    ReadClass(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
+            System.out.printf("After read: i = %d, x = %f, y = %f\n", i, tabulatedFunction.getX(i), tabulatedFunction.getY(i));
+        }
+    }
+}
