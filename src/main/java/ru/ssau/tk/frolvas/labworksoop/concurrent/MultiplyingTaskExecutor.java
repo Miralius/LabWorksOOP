551c4ac9f8525f0e67da930cb429e279a6373771
Frolov <f-miralius@yandex.ru>
2019-12-07 09:27:48 +0400

    LW#4.1 → Y task: Created main-class MultiplyingTaskExecutor

---
+++ src/main/java/ru/ssau/tk/frolvas/labworksoop/concurrent/MultiplyingTaskExecutor.java
@@ -0,0 +1,21 @@
+package ru.ssau.tk.frolvas.labworksoop.concurrent;
+
+import ru.ssau.tk.frolvas.labworksoop.functions.*;
+
+import java.util.*;
+
+public class MultiplyingTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction function = new LinkedListTabulatedFunction(new UnitFunction(), 0, 1000, 10000);
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < Runtime.getRuntime().availableProcessors() + 1; i++) {
+            MultiplyingTask task = new MultiplyingTask(function);
+            threads.add(new Thread(task));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        Thread.sleep(2000);
+        System.out.println(function);
+    }
+}
