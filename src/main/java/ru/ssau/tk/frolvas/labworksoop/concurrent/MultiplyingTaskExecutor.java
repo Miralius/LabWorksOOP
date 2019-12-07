956a00e57e4584300a48c5d8e87893e5a2cead0c
Frolov <f-miralius@yandex.ru>
2019-12-07 09:46:26 +0400

    LW#4.1 → Y task: Synchronized MultiplyingTaskExecutor

--- src/main/java/ru/ssau/tk/frolvas/labworksoop/concurrent/MultiplyingTaskExecutor.java
+++ src/main/java/ru/ssau/tk/frolvas/labworksoop/concurrent/MultiplyingTaskExecutor.java
@@ -5,12 +5,15 @@ import ru.ssau.tk.frolvas.labworksoop.functions.*;
 import java.util.*;
 
 public class MultiplyingTaskExecutor {
+    @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
     public static void main(String[] args) throws InterruptedException {
         TabulatedFunction function = new LinkedListTabulatedFunction(new UnitFunction(), 0, 1000, 10000);
         List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < Runtime.getRuntime().availableProcessors() + 1; i++) {
             MultiplyingTask task = new MultiplyingTask(function);
-            threads.add(new Thread(task));
+            synchronized (task) {
+                threads.add(new Thread(task));
+            }
         }
         for (Thread thread : threads) {
             thread.start();
