c7441d844789e222e1be3363426fbc50ecdce3c1
Vasilieva <masha.maria.vas@bk.ru>
2019-12-20 10:11:08 +0400

    draft of window , class for the note to be entered in the table

---
+++ src/main/java/ru/ssau/tk/frolvas/labworksoop/ui/Window.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.frolvas.labworksoop.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Window extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        //redefine me!
+    }
+
+}
